5281f68d5ff1ad9ee743e671c675156f658d3281
Kelliott <52241744+kelliott14@users.noreply.github.com>
2022-04-25 16:29:31 +1000

    nav bar loading

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -59,4 +59,28 @@
 
 // Set sections as active
 
+const sectionHeaders = document.querySelectorAll('.landing__container')
+const navBarList = document.getElementById('navbar__list')
+console.log(navBarList)
+
+for(let i = 0; i < sectionHeaders.length; i++){
+    const idLink = sectionHeaders[i].parentElement.id;
+    const headerText = sectionHeaders[i].querySelector('.sectionHeader').innerHTML;
+    
+    const newLi = document.createElement('li');
+    const newA = document.createElement('a');
+
+    newA.textContent = headerText;
+    newA.setAttribute('href', "#" + idLink);
+    newLi.appendChild(newA);
+    navBarList.appendChild(newLi)
+
+    
+}
+
+
+
+
+
+
 
