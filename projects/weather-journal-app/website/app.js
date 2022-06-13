bd655ab299cff61900c6f39ca0a24cea0682a838
Kelliott <52241744+kelliott14@users.noreply.github.com>
2022-06-13 14:33:18 +1000

    api running

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,10 +1,12 @@
 /* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = '6e06782fcbeceda142b7bbf675c03371'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-
+// post data
 const postData = async ( url = '', data = {})=>{
     console.log(data);
       const response = await fetch(url, {
@@ -13,7 +15,7 @@ const postData = async ( url = '', data = {})=>{
       headers: {
           'Content-Type': 'application/json',
       },
-     // Body data type must match "Content-Type" header        
+       
       body: JSON.stringify(data), 
     });
 
@@ -30,3 +32,25 @@ postData('/addData', {
     temp: '15',
     date: '01/05/22',
     content: 'testing 123' });
+
+// fetch API data
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+const newZip =  document.getElementById('zip').value;
+getWeather(baseURL, newZip, apiKey)
+
+}
+const getWeather = async (baseURL, zip, key)=>{
+
+  const res = await fetch(baseURL+zip+',us&appid='+key)
+  try {
+
+    const data = await res.json();
+    console.log(data)
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
